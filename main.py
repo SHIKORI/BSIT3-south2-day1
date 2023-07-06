8fa002b7b2b34df54e0e175a9b3f8cf4774f8444
MILES FELICIANO <115053837+SHIKORI@users.noreply.github.com>
2023-07-06 15:20:29 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+num1= 16
+num2= 22
+
+sum = num1 + num2
+print("SUM = {0}".format(sum))
+
+diff = num1 - num2
+print("Difference = {0}".format(diff))
+
+product = num1 * num2
+print("Product = {0}".format(product))
+
+quot = num1 / num2
+print("Qoutient = {0}".format(quot))
